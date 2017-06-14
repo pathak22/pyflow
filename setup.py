76bbcbb1d9c0e61bbbf048ddb4f9b4fa1de8017b
Thiemo Alldieck <alldieck@cg.cs.tu-bs.de>
2017-06-14 17:43:45 +0200

    fix numpy includes for custom numpy location e.g. homebrew

--- setup.py
+++ setup.py
@@ -9,13 +9,16 @@ from Cython.Build import cythonize
 from distutils.extension import Extension
 from glob import glob
 
+import numpy
+
 sourcefiles = ['pyflow.pyx', ]
 sourcefiles.extend(glob("src/*.cpp"))
-extensions = [Extension("pyflow", sourcefiles)]
+extensions = [Extension("pyflow", sourcefiles, include_dirs=[numpy.get_include()])]
 setup(
     name="pyflow",
     version="1.0",
     description="Python wrapper for the Coarse2Fine Optical Flow code.",
     author="Deepak Pathak",
     ext_modules=cythonize(extensions),
+    include_dirs=[numpy.get_include()]
 )
